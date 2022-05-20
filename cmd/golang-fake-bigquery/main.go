a1b6a61faeeeef821b75a9616f643a4da65368fd
Joselito Viveiros Nogueira Filho <joselitofilhoo@gmail.com>
2022-05-20 14:13:18 +0100

    chore: main file code improvements.

--- cmd/golang-fake-bigquery/main.go
+++ cmd/golang-fake-bigquery/main.go
@@ -10,42 +10,73 @@ import (
 	"os"
 	"strconv"
 
+	"github.com/joselitofilho/golang-fake-bigquery/internal/logging"
 	"github.com/joselitofilho/golang-fake-bigquery/pkg/routes"
 )
 
 func main() {
-	defaultPort, _ := strconv.Atoi(os.Getenv("FAKE_BQ_PORT"))
+	var discoveryJsonPath string
+	var portNum int
 
-	discoveryJsonPath := flag.String("discovery-json-path", "", "path to discovery.json")
-	portNum := flag.Int("port", defaultPort, "port number to listen at")
+	if err := buildConfig(&discoveryJsonPath, &portNum); err != nil {
+		logging.Error.Println(err)
+		os.Exit(1)
+	}
+
+	discoveryJson, err := buildDiscoveryJson(discoveryJsonPath, portNum)
+	if err != nil {
+		logging.Error.Println(err)
+		os.Exit(1)
+	}
+
+	if err := listenAndServe(discoveryJson, portNum); err != nil {
+		logging.Error.Println(err)
+		os.Exit(1)
+	}
+}
+
+func buildConfig(discoveryJsonPath *string, port *int) error {
+	envPort, _ := strconv.Atoi(os.Getenv("FAKE_BQ_PORT"))
+
+	discoveryJsonPathFlag := flag.String("discovery-json-path", "", "path to discovery.json")
+	portFlag := flag.Int("port", envPort, "port number to listen at")
 	flag.Parse()
 
-	if *discoveryJsonPath == "" {
-		log.Fatalf("Please specify -discovery-json-path")
-	}
-	if *portNum == 0 {
-		log.Fatalf("Please specify -port")
+	if *discoveryJsonPathFlag == "" {
+		return fmt.Errorf("please specify -discovery-json-path")
 	}
 
-	var err error
-	discoveryJson, err := ioutil.ReadFile(*discoveryJsonPath)
-	if err != nil {
-		panic(err)
+	if *portFlag == 0 {
+		return fmt.Errorf("please specify -port")
 	}
-	myUrl := fmt.Sprintf("http://localhost:%d", *portNum)
+
+	*discoveryJsonPath = *discoveryJsonPathFlag
+	*port = *portFlag
+
+	return nil
+}
+
+func buildDiscoveryJson(path string, port int) ([]byte, error) {
+	discoveryJson, err := ioutil.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	myUrl := fmt.Sprintf("http://localhost:%d", port)
 	discoveryJson = bytes.Replace(discoveryJson,
 		[]byte("https://www.googleapis.com"), []byte(myUrl), -1)
 
-	listenAndServe(discoveryJson, *portNum)
+	return discoveryJson, nil
 }
 
-func listenAndServe(discoveryJson []byte, portNum int) {
+func listenAndServe(discoveryJson []byte, portNum int) error {
 	app := routes.NewApp(discoveryJson)
 	http.HandleFunc("/", app.Route)
 
 	log.Printf("Listening on :%d...", portNum)
-	err := http.ListenAndServe(fmt.Sprintf(":%d", portNum), nil)
-	if err != nil {
-		log.Fatal("ListenAndServe: ", err)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", portNum), nil); err != nil {
+		return fmt.Errorf("ListenAndServe: %v", err)
 	}
+
+	return nil
 }
